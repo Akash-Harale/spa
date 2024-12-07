04fd8ebe77f6d1ee564c68481c9216432ded46a3
Akash Harale <akash.v.harale@gmail.com>
2024-12-08 01:08:45 +0530

    added background of lines, like the background of testimonials

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="absolute inset-0 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px] pointer-events-none" />
+        {children}
+      </body>
     </html>
   )
 }
